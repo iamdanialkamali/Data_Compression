dfabb10e45feff6e81c15f996cc0115ff5fb5b57
Danielka <daniel.kamali@yahoo.com>
2017-11-30 22:53:09 +0330

    phase 2 starter

--- main.cpp
+++ main.cpp
@@ -1030,7 +1030,7 @@ void xmlparser1(string path){
         input>>c;
         switch(c){
             case '<':
-                char buffer[4096];
+                char buffer[4096000];
                 char w='s' ;
                 input>>w;
 
@@ -1043,26 +1043,26 @@ void xmlparser1(string path){
                     if(w=='e'){
                         cout << "Head:";
                         input.seekp(3, ios::cur);
-                        input.get(buffer, 4096, '<');
+                        input.get(buffer, 4096, '>');
                         cout << buffer << '\n';
                     }
                     if(w=='o'){
                         cout << "Host:";
                         input.seekp(3, ios::cur);
-                        input.get(buffer, 4096, '<');
+                        input.get(buffer, 4096, '>');
                         cout << buffer << '\n';
                     }
                 }
                 if (w=='T'){
                     cout<<"Time:";
                     input.seekp(5,ios::cur);
-                    input.get(buffer,4096,'<');
+                    input.get(buffer,4096,'>');
                     cout<<buffer<<'\n';
                 }
                 if (w=='U'){
                     cout<<"UserAgent:";
                     input.seekp(9,ios::cur);
-                    input.get(buffer,4096,'<');
+                    input.get(buffer,4096,'>');
                     cout<<buffer<<'\n';
                 }
                 if (w=='R'){
@@ -1071,17 +1071,17 @@ void xmlparser1(string path){
                     if(w=='f'){
                         cout<<"Referer:";
                         input.seekp(5,ios::cur);
-                        input.get(buffer,4096,'<');
+                        input.get(buffer,409600000,'>');
                         cout<<buffer<<'\n';
                     } else if(w=='s'){
                         cout<<"Response Code:";
                         input.seekp(10,ios::cur);
-                        input.get(buffer,4096,'<');
+                        input.get(buffer,4096,'>');
                         cout<<buffer<<'\n';
                     }
                     else if(w=='q'){ cout<<"RequestSize:";
                         input.seekp(9,ios::cur);
-                        input.get(buffer,4096,'<');
+                        input.get(buffer,4096,'>');
                         cout<<buffer<<'\n';
                     }
 
